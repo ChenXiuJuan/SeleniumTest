3b7e78cb741a35022ce948d39241a1c900341b98
xiujuan.chen <1724022238@qq.com>
2020-08-20 18:17:36 +0800

    蜂场地图-外部蜂场测试case

--- sql/FarmMapSql.py
+++ sql/FarmMapSql.py
@@ -124,3 +124,74 @@ class FarmMap(object):
                    AND tbf.is_delete = 0
                    AND tur.is_delete = 0;"""
         return self.db.operate(host_ip, sql)
+
+    def query_swarm_info(self, plant_name, create_time, user_name):
+
+        """
+        外部蜂场信息
+        :param plant_name: 当前蜜源名称
+        :param create_time: 创建时间
+        :param user_name: 采集人真实姓名
+        :return:
+        """
+        sql = """SELECT
+                    i.*
+                FROM
+                    `fc-bee`.t_swarm_info i
+                    LEFT JOIN `world-user`.t_user u ON i.creator_id = u.id
+                WHERE
+                    i.is_delete = 0 
+                    AND i.cur_nectar_type IN (SELECT sp.`code` FROM `fc-bee`.t_nectar_source_plant sp WHERE sp.is_delete = 0 AND sp.plant_name = '%s') 
+                    AND i.create_time LIKE '%s%%%%'
+                    AND u.username = '%s';""" %(plant_name, create_time, user_name)
+        return self.db.operate(host_ip, sql)
+
+    def query_config_value(self, code, value):
+
+        """
+        查询配置文件value
+        :param code: 配置类型
+        :param value:配置key
+        :return:
+        """
+        sql = """SELECT
+                    c.`value`
+                FROM
+                    `fc-bee`.t_config c
+                WHERE
+                    c.is_delete = 0
+                    AND c.`code` = '%s'
+                    AND c.`key` = '%s';""" %(code, value)
+        return self.db.operate(host_ip, sql)
+
+    def query_swarm_info_img(self, swarm_id):
+        """
+        查询外部蜂场照片信息
+        :param swarm_id: 外部蜂场id
+        :return:
+        """
+        sql = """SELECT
+                    a.url
+                FROM
+                    `fc-bee`.t_swarm_attach a 
+                WHERE
+                    a.is_delete = 0
+                    AND a.swarm_id = '%s';""" %swarm_id
+        return self.db.operate(host_ip, sql)
+
+    class Bee(object):
+        L = Log("ConfigInformationSql")
+        db = DataBaseOperate()
+
+        def query_swarm_count(self):
+            """
+            查询外部蜂场数量
+            :return:
+            """
+            sql = """SELECT
+                            Count( * ) 
+                        FROM
+                            `fc-bee`.t_swarm_info si 
+                        WHERE
+                            si.is_delete = 0;"""
+            return self.db.operate(host_ip, sql)
